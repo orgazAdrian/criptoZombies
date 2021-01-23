a78a3d5af98ec3dc4327d02cddb2cf6bf4e88ad2
Adrian Orgaz <63147655+orgazAdrian@users.noreply.github.com>
2021-01-23 12:58:57 +0100

    Update zombiehelper.sol

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -2,11 +2,25 @@ pragma solidity ^0.4.19;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
+
+  uint levelUpFee = 0.001 ether;
+  
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
   
+  function withdraw() external onlyOwner {
+    owner.transfer(this.balance);
+  } 
+  function setLevelUpFee(uint _fee) external payable onlyOwner {
+    levelUpFee= _fee;
+  }
+  
+  function levelUp(uint _zombieId) external payable {
+  require(msg.sender == levelUpFee);
+  zombies[_zombieId].level++;
+  }
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
